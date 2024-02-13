a9e39353efa4ecc2e1269615321fb352d945e1a0
Aharon Maghen <aharonmaghen@gmail.com>
2024-02-13 11:34:15 +0200

    Initial commit

---
+++ src/main/java/com/movienight/movienightbackend/MovienightBackendApplication.java
@@ -0,0 +1,13 @@
+package com.movienight.movienightbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovienightBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovienightBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movienight/movienightbackend/MovienightBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movienight.movienightbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovienightBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
