dff7d4a13213e3546ae8b505ad1437b0ef1012b3
Aharon Maghen <aharonmaghen@gmail.com>
2024-02-15 19:31:58 +0200

    Remove controller and repository

--- src/main/java/com/movienight/movienightbackend/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.movienight.movienightbackend.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.movienight.movienightbackend.models.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-  
-}
