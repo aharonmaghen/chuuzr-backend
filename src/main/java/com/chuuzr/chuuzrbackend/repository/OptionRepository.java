65d994e81dbbbd0193f03a04e2704d0757a226f3
Aharon Maghen <aharonmaghen@gmail.com>
2025-11-09 13:35:48 +0200

    Add new endpoint to get all options with x option type uuid

--- src/main/java/com/chuuzr/chuuzrbackend/repository/OptionRepository.java
+++ src/main/java/com/chuuzr/chuuzrbackend/repository/OptionRepository.java
@@ -1,5 +1,7 @@
 package com.chuuzr.chuuzrbackend.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.chuuzr.chuuzrbackend.model.Option;
@@ -9,4 +11,6 @@ import java.util.UUID;
 
 public interface OptionRepository extends JpaRepository<Option, Long> {
   Optional<Option> findByUuid(UUID uuid);
+
+  Page<Option> findByOptionTypeUuid(UUID optionTypeUuid, Pageable pageable);
 }
