3bb76fe219b5999672f3d4e52e8b3f83ec0c9674
Aharon Maghen <aharonmaghen@gmail.com>
2026-02-15 14:46:21 +0200

    Redo docker

--- src/main/resources/db/migration/V3__Add_OptionType_To_Rooms.sql
+++ src/main/resources/db/migration/V3__Add_OptionType_To_Rooms.sql
@@ -1,16 +1,16 @@
 BEGIN;
 
 -- Add the column as nullable first
-ALTER TABLE public.rooms 
+ALTER TABLE public.rooms
 ADD COLUMN option_type_id bigint;
 
 -- Set existing data to your default (ID = 1)
-UPDATE public.rooms 
-SET option_type_id = 1 
+UPDATE public.rooms
+SET option_type_id = 1
 WHERE option_type_id IS NULL;
 
 -- Now enforce NOT NULL and add the Foreign Key
-ALTER TABLE public.rooms 
+ALTER TABLE public.rooms
 ALTER COLUMN option_type_id SET NOT NULL;
 
 ALTER TABLE public.rooms
